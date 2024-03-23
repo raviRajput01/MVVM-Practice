a0dee5d8168e22d3de6cdf5df1b795e6ed442f95
Ravi Kumar Singh <raviios2015@gmail.com>
2024-03-24 00:09:15 +0530

    Initial Commit

---
+++ MVVM Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Practice
+//
+//  Created by Ravi Kumar Singh on 24/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
